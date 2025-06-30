e6b24252effce54b590aefb2a69a689d22c3bdea
donggori2025 <donggori2025@gmail.com>
2025-06-30 11:40:12 +0900

    불필요한 import 삭제 (ESLint no-unused-vars 오류 해결)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 "use client";
-import Image from "next/image";
-import Link from "next/link";
-import { Card, CardContent } from "@/components/ui/card";
 import React from "react";
-import { motion } from "framer-motion";
 
 // 메인 페이지 컴포넌트
 export default function Home() {
