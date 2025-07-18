1e159bc235889350453851a17382a6eb8e8dd390
donggori2025 <donggori2025@gmail.com>
2025-07-18 18:20:35 +0900

    공지사항 리스트 map idx 미사용 변수 제거 (빌드 에러 수정)

--- .next/static/chunks/_8adce68f._.js
+++ .next/static/chunks/_8adce68f._.js
@@ -165,7 +165,7 @@ function NoticesPage() {
                         fileName: "[project]/app/notices/page.tsx",
                         lineNumber: 46,
                         columnNumber: 13
-                    }, this) : sorted.map((notice, idx)=>/*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("li", {
+                    }, this) : sorted.map((notice)=>/*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("li", {
                             className: "grid grid-cols-12 items-center py-3 border-b border-gray-100 hover:bg-gray-50 transition group",
                             children: [
                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
