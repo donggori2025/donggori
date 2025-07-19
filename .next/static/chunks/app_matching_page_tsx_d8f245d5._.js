3a5b2a0644e2535d21bba7fd15036380e2ef3402
donggori2025 <donggori2025@gmail.com>
2025-07-19 19:54:14 +0900

    fix: .next 폴더 제거 및 .gitignore 업데이트
    - .next 폴더를 Git에서 제거하여 큰 파일 문제 해결
    - .gitignore에 Next.js 관련 파일들 추가
    - GitHub Push Protection 문제 해결

--- .next/static/chunks/app_matching_page_tsx_d8f245d5._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_@supabase_node-fetch_browser_b1e3b627.js",
-  "static/chunks/node_modules_b3b81a04._.js",
-  "static/chunks/_08148318._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_terms_[type]_page_tsx_d8f245d5._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_terms_[type]_page_tsx_eba7349e._.js"
-],
-    source: "dynamic"
-});
