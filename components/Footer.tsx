f830c4737d90ff18e23cab055912e3388763ece0
donggori2025 <donggori2025@gmail.com>
2025-08-27 14:53:37 +0900

    Footer 협회명 수정: (사) → 사단법인으로 변경

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -39,7 +39,7 @@ const Footer = () => {
         </nav>
         {/* 협회 정보 */}
         <div className="text-xs text-gray-500 mb-1 text-center px-4">
-          (사)동대문구의류봉제산업연합회
+          사단법인 동대문구의류봉제산업연합회
         </div>
         {/* 저작권 문구 */}
         <div className="text-xs text-gray-400 text-center">
